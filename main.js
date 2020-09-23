82c9b78fa77873f7b63bf3f62ea4a99325e454e9
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2020-09-23 15:54:16 +0100

    Can Wake Up

--- main.js
+++ main.js
@@ -5,4 +5,28 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.once('ready', () => {
+    var generalChannel = client.channels.cache.get("758267928033361925") // Replace with known channel ID
+    generalChannel.send("*Yawn...* Good Morning. Bot Online.")  
+});
+
+client.on("message", function (msg) {
+	// if message begins with "ping"
+	if (msg.content.indexOf("Hello Digram") === 0) {
+		// send a message to the channel the ping message was sent in.
+
+		user = message.member;
+		user = user.toString();
+		DigramMessage = ":wave:" + user;
+		client.sendMessage(msg.channel, "Hi.");
+		
+
+		// alert the console
+		console.log("pong-ed " + msg.author.username);
+	}
+});
+
+
+
+
 client.login('NzU4MzEzOTcwNTA2Mzk5NzU0.X2tI6w.YiJ8BUUCj8pU8Fy45vbeP_bW7r4');
