85fb9b01f438404d8b7fffd2b1993f48efe9a53f
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2020-09-23 15:29:37 +0100

    Online and Working

--- main.js
+++ main.js
@@ -1,38 +1,8 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
-  
-  client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-  });
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.login('NzU4MzEzOTcwNTA2Mzk5NzU0.X2tI6w.vyUr68PXS8frvs0Pg6-yxk48TmM')
-
+client.login('NzU4MzEzOTcwNTA2Mzk5NzU0.X2tI6w.YiJ8BUUCj8pU8Fy45vbeP_bW7r4');
