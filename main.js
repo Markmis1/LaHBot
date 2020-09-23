27db599f00a983127b17d3c57fc4bcee5829c5aa
Markmis1 <37936399+Markmis1@users.noreply.github.com>
2020-09-23 14:30:33 +0100

    Version 0.5
    V0.5

---
+++ main.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready' , () =>{
+    console.log('Digram is Online. Hello Eros.')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('NzU4MzEzOTcwNTA2Mzk5NzU0.X2tI6w.vyUr68PXS8frvs0Pg6-yxk48TmM')
+
